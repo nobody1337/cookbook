e13bcb26fe91a5e059e5de4e2697e3be24c00164
nobody1337 <98647065+nobody1337@users.noreply.github.com>
2022-05-11 13:22:41 +0200

    update recipeCollection.html

--- src/main/java/de/cookbook/cookbook/controller/RecipeController.java
+++ src/main/java/de/cookbook/cookbook/controller/RecipeController.java
@@ -5,8 +5,11 @@ import de.cookbook.cookbook.model.Recipe;
 import de.cookbook.cookbook.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class RecipeController {
@@ -30,7 +33,8 @@ public class RecipeController {
     }
 
     @GetMapping("/recipeCollection")
-    public String recipeCol(){
+    public String recipeCol(Model model){
+        model.addAttribute("recipe",recipeRepository.findAll());
         return "recipeCollection";
     }
 
